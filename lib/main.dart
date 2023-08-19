4a0d24ad35b5c88fd831810188d7f79cd7ecafff
Bereket <berekettadege09@gmail.com>
2023-08-19 18:13:24 +0300

    global cubit provided

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_chat/cubit/fetch_conversation_message_cubit/index.dart';
 import 'package:firebase_chat/cubit/firebase_auth_cubit/index.dart';
+import 'package:firebase_chat/resources/conversation_service.dart';
 import 'package:firebase_chat/screens/splash_screen/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -14,17 +16,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: BlocProvider<FirebaseAuthBloc>(
-        child: SplashScreen(),
-        create: (context) => FirebaseAuthBloc(),
-      ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<FirebaseAuthBloc>(
+          create: (context) => FirebaseAuthBloc(),
+        ),
+        BlocProvider<FetchConversationCubit>(
+          create: (context) => FetchConversationCubit(
+              conversationService: ConversationService()),
+        ),
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: SplashScreen()),
     );
   }
 }
